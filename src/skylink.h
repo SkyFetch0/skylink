f12de6f71f1bcfb22821eed1b7b2c44f02a06d07
Halil <156548013+SkyFetch0@users.noreply.github.com>
2024-06-01 02:15:42 +0300

    Update skylink.h

--- src/skylink.h
+++ src/skylink.h
@@ -1,32 +1,16 @@
 /**
  * @file       skylink.h
+ * @brief      skylink protocol definitions
  * @author     skyfetch
  * @license    This project is released under the MIT License (MIT)
- * @copyright  Copyright (c) 2024 skyfetch
+ * @copyright  Copyright (c) 2024 skyfetch CO
  * @date       Jan 2024
- * @brief      skylink protocol definitions
- *
  */
 
 #ifndef skylink_h
 #define skylink_h
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 
-class skylink {
-  public:
-    skylink(const char* host, uint16_t port,const char* auth);
-    void connect();
-    void disconnect();
-    bool isConnected();
-    void sendData(const char* data);
-    String receiveData();
-  
-  private:
-    const char* _host;
-    uint16_t _port;
-    const char* _auth; 
-    WiFiClient _client;
-    bool _connected;
-};
+#include <skylink/skylinkApi.h>
+#warning "Please include a board-specific header file, instead of Blynk.h (see examples)"
+
 #endif
